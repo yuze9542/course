bcc6a28206588329a387a9850d8852867f08b515
yuze <278905642@qq.com>
2020-07-25 00:41:00 +0800

    1 添加了mybatis生成器 2 测试了一下mybatis生成器

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,8 @@
 package com.course.server.service;
 
 import com.course.server.domain.NewTable;
+import com.course.server.domain.NewTableExample;
+import com.course.server.mapper.NewTableMapper;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,12 @@ import java.util.List;
 @Service
 public class TestService {
     @Resource
-    private TestMapper testMapper;
+    private NewTableMapper newTableMapper;
 
     public List<NewTable> list(){
-        return testMapper.list();
+        NewTableExample newTableExample = new NewTableExample();
+        newTableExample.createCriteria().andIdEqualTo("1");  //真正的where条件
+//        newTableExample.setOrderByClause("id desc");  //到处排列
+        return newTableMapper.selectByExample(newTableExample);
     }
 }
