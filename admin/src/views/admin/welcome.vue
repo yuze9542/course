2d55c3b88242bb0eb349c41f33b990f93849aa02
yuze <278905642@qq.com>
2020-07-25 13:24:48 +0800

    1 增加public/index.html 的js 从ace/index的js复制

---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>你好,欢迎进入到welcome页面</h1>
+  </div>
+</template>
--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,7 @@
                             <span class="lbl"> 记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button @click="login()" type="button" class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -75,10 +75,20 @@
 </template>
 
 <script>
-    $('body').attr('class', 'login-layout light-login');
+
     export default {
-        name: 'App',
+        name: 'login',
+        methods: {
+            login() {
+                this.$router.push("/admin")
+            },
+        },
+        mounted: function () {
+            $('body').removeClass('no-skin');
+            $('body').attr('class', 'login-layout light-login');
+        }
     }
+
 </script>
 
 <style>
