26f00f58e6ae410a1fdd77b176d03d491c5dccf3
yuze <278905642@qq.com>
2020-07-25 11:53:12 +0800

    增加admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -1,78 +1,77 @@
 <template>
-    <div class="main-container">
-        <div class="main-content">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="login-container">
-                        <div class="center">
-                            <h1>
-                                <i class="ace-icon fa fa-leaf green"></i>
-                                <span class="black" id="id-text2">登录页面</span>
-                            </h1>
-                        </div>
+  <div class="main-container">
+    <div class="main-content">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="login-container">
+            <div class="center">
+              <h1>
+                <i class="ace-icon fa fa-leaf green"></i>
+                <span class="black" id="id-text2">登录页面</span>
+              </h1>
+            </div>
 
-                        <div class="space-6"></div>
+            <div class="space-6"></div>
 
-                        <div class="position-relative">
-                            <div id="login-box" class="login-box visible widget-box no-border">
-                                <div class="widget-body">
-                                    <div class="widget-main">
-                                        <h4 class="header blue lighter bigger">
-                                            <i class="ace-icon fa fa-coffee green"></i>
-                                            请输入用户名和密码
-                                        </h4>
+            <div class="position-relative">
+              <div id="login-box" class="login-box visible widget-box no-border">
+                <div class="widget-body">
+                  <div class="widget-main">
+                    <h4 class="header blue lighter bigger">
+                      <i class="ace-icon fa fa-coffee green"></i>
+                      请输入用户名和密码
+                    </h4>
 
-                                        <div class="space-6"></div>
+                    <div class="space-6"></div>
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
+                    <form>
+                      <fieldset>
+                        <label class="block clearfix">
                           <span class="block input-icon input-icon-right">
-                              <input type="text" class="form-control" placeholder="Username" />
+                              <input type="text" class="form-control" placeholder="Username"/>
                               <i class="ace-icon fa fa-user"></i>
                           </span>
-                                                </label>
+                        </label>
 
-                                                <label class="block clearfix">
+                        <label class="block clearfix">
                           <span class="block input-icon input-icon-right">
-                              <input type="password" class="form-control" placeholder="Password" />
+                              <input type="password" class="form-control" placeholder="Password"/>
                               <i class="ace-icon fa fa-lock"></i>
                           </span>
-                                                </label>
+                        </label>
 
-                                                <div class="space"></div>
+                        <div class="space"></div>
 
-                                                <div class="clearfix">
-                                                    <label class="inline">
-                                                        <input type="checkbox" class="ace" />
-                                                        <span class="lbl"> 记住我</span>
-                                                    </label>
+                        <div class="clearfix">
+                          <label class="inline">
+                            <input type="checkbox" class="ace"/>
+                            <span class="lbl"> 记住我</span>
+                          </label>
 
-                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
-                                                        <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">登录</span>
-                                                    </button>
-                                                </div>
+                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                            <i class="ace-icon fa fa-key"></i>
+                            <span class="bigger-110">登录</span>
+                          </button>
+                        </div>
 
-                                                <div class="space-4"></div>
-                                            </fieldset>
-                                        </form>
-
-
-                                        <div class="space-6"></div>
-
-                                    </div><!-- /.widget-main -->
-                                </div><!-- /.widget-body -->
-                            </div><!-- /.login-box -->
+                        <div class="space-4"></div>
+                      </fieldset>
+                    </form>
 
 
+                    <div class="space-6"></div>
 
-                        </div><!-- /.position-relative -->
-                    </div>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.main-content -->
-    </div><!-- /.main-container -->
+                  </div><!-- /.widget-main -->
+                </div><!-- /.widget-body -->
+              </div><!-- /.login-box -->
+
+
+            </div><!-- /.position-relative -->
+          </div>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.main-content -->
+  </div><!-- /.main-container -->
 </template>
 
 <script>
