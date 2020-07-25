d6ebd1dc9d83d21dcb6843c3dab399ac13a5afa9
yuze <278905642@qq.com>
2020-07-26 00:11:33 +0800

    1 增加dto层 用于controller和service层 2 增加 ChapterDto 3 修改 ChapterService 将返回Chapter 改成返回ChapterDto

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,7 +1,8 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
 import com.course.server.domain.Chapter;
 import com.course.server.domain.NewTable;
+import com.course.server.dto.ChapterDto;
 import com.course.server.service.ChapterService;
 import com.course.server.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import java.util.List;
 //如果返回的是json格式的数据 用RestController
 //如果返回的是页面 就是Controller
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     ChapterService chapterService ;
 
     @RequestMapping("/chapter")
-    public List<Chapter> test(){
+    public List<ChapterDto> test(){
        return chapterService.list();
     }
 }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,7 +1,6 @@
 --大章
 
 drop table if exists `chapter`;
-drop table if exists `chaper`;
 create table `chapter`(
     `id` char(8) not null comment 'ID',
     `course_id` char(8) comment '课程ID',
@@ -9,7 +8,8 @@ create table `chapter`(
     primary key(`id`)
 )engine=innodb default charset=utf8mb4 comment='大章';
 
-
+insert into `chapter`(id, course_id, name) VALUES ('00000000','00000000','测试大章 1');
+insert into `chapter`(id, course_id, name) VALUES ('00000001','00000000','测试大章 2');
 
 
 --测试
