8f8114607f3f7f8cfd926692e1e4f4a47ded7c39
yuze <278905642@qq.com>
2020-07-28 01:34:25 +0800

    1 生成器开发 增加dto生成

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -1,36 +1,52 @@
 package com.course.generator.server;
+import com.course.generator.util.DbUtil;
+import com.course.generator.util.Field;
+import com.course.generator.util.FreemarkerUtil;
 
-import com.course.generator.util.FreeMarkerUtil;
-import freemarker.template.TemplateException;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ServerGenerator {
 
     static String MODULE = "business";
+    static String toDtoPath = "server/src/main/java/com/course/server/dto/";
     static String toServicePath = "server/src/main/java/com/course/server/service/";
     static String toControllerPath = "business/src/main/java/com/course/"+MODULE+"/controller/admin/";
 
-    public static void main(String[] args) throws IOException, TemplateException {
+    public static void main(String[] args) throws Exception {
 
         String Domain = "Section";
         String domain = "section";
         String tableNameCn = "小节";
         String module = MODULE;
+        List<Field> fieldList = DbUtil.getColumnByTableName(domain);//  获取所有列信息
+        Set<String> typeSet = getJavaTypes(fieldList);  //根据列信息获取集合
         Map<String,Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
         map.put("tableNameCn",tableNameCn);
         map.put("module",module);
+        map.put("fieldList",fieldList);
+        map.put("typeSet",typeSet);
+
+        //生成Dto
+        FreemarkerUtil.initConfig("dto.ftl");
+        FreemarkerUtil.generator(toDtoPath+Domain+"Dto.java",map);
 
         //生成Service
-        FreeMarkerUtil.initConfig("service.ftl");
-        FreeMarkerUtil.generator(toServicePath+Domain+"Service.java",map);
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(toServicePath+Domain+"Service.java",map);
 
         //生成Controller
-        FreeMarkerUtil.initConfig("controller.ftl");
-        FreeMarkerUtil.generator(toControllerPath+Domain+"Controller.java",map);
+        FreemarkerUtil.initConfig("controller.ftl");
+        FreemarkerUtil.generator(toControllerPath+Domain+"Controller.java",map);
+    }
+
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
--- generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
+++ generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
@@ -11,27 +11,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 
+public class FreemarkerUtil {
 
-public class FreeMarkerUtil {
+    static String ftlPath = "generator/src/main/java/com/course/generator/ftl/";
 
-    static String ftlPath = "generator/src/main/java/com/course/generator/ftl/";   //模板路径
     static Template temp;
 
     public static void initConfig(String ftlName) throws IOException {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);    //选择包版本
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
         cfg.setDirectoryForTemplateLoading(new File(ftlPath));
         cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
-        temp = cfg.getTemplate(ftlName);        //上面四个都是读模板
-
+        temp = cfg.getTemplate(ftlName);
     }
 
-    public static void generator(String fileName, Map<String,Object> map) throws IOException, TemplateException {
-        FileWriter fw = new FileWriter(fileName);//生成什么文件
-        BufferedWriter bw = new BufferedWriter(fw);//   //放到buffer
-        temp.process(map, bw); //map是放变量的地方 生成中...
+    public static void generator(String fileName, Map<String, Object> map) throws IOException, TemplateException {
+        FileWriter fw = new FileWriter(fileName);
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(map, bw);
         bw.flush();
         fw.close();
     }
-
-
 }
